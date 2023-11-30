a5767fbde02d6b6779794331ed0970f36d8f2f2c
abdoemr11 <abdoemr11@gmail.com>
2023-11-30 08:25:39 +0200

    imported theme settings from realtimecolor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ const config: Config = {
                 "gradient-conic":
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
+            colors: {
+                text: "rgb(var(--text))",
+                background: "rgb(var(--background))",
+                primary: "rgb(var(--primary))",
+                secondary: "rgb(var(--secondary))",
+                accent: "rgb(var(--accent))",
+            },
+            fontSize: {
+                sm: "0.800rem",
+                base: "1rem",
+                xl: "1.250rem",
+                "2xl": "1.563rem",
+                "3xl": "1.954rem",
+                "4xl": "2.442rem",
+                "5xl": "3.053rem",
+            },
+            boxShadow: {
+                lg: "0 10px 15px -3px rgb(var(--secondary) / .1), 0 4px 6px -4px rgb(var(--secondary) / .1)",
+            },
         },
     },
     plugins: [require("@tailwindcss/typography")],
